382b301d5bc1e79e78ab5e06dd2dee643b23caba
David Johnson <djcomlab@gmail.com>
2016-03-03 16:40:01 +0000

    refactor to refer to json2isatab_docker project as submodule, in order to keep Jenkins builds on specific microservices separated correctly

--- json2isatab/run_json2tab.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-src_json = sys.argv[1]
-target_dir = sys.argv[2]
-try:
-    from isatools.convert import json2isatab
-except ImportError as e:
-    raise RuntimeError("Could not import isatools package")
-json2isatab.convert(open(src_json), target_dir)
